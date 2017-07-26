4c45327fbf94d6dca770b4ebe04d8785fad828c5
Tom Klaasen <tomk@tomk.be>
2017-07-26 17:27:31 +0200

    tinkering with timestamps

--- main.rb
+++ main.rb
@@ -6,10 +6,16 @@ require "bundler/setup"
 require "viddl"
 require "viddl/video/clip"
 
+def retrieve_time(photopath)
+  File.birthtime(photopath)
+end
+
 def cut_video(path, start, duration)
   options = {:start => start, :duration => duration}
   Viddl::Video::Clip.process path, options
 end
 
 
-cut_video('/Users/tomklaasen/Desktop/VIRB\ copy/VIRB_0629.MP4', 268, 30)
+phototime = retrieve_time('/Users/tomklaasen/Desktop/VIRB copy/VIRB_0630.jpg')
+puts phototime
+# cut_video('/Users/tomklaasen/Desktop/VIRB\ copy/VIRB_0629.MP4', phototime - 15, 30)
