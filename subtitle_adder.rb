2ca4312346a901b88ca30211a23c19b0449e4980
Tom Klaasen <tomk@tomk.be>
2017-10-25 00:11:49 +0200

    making progress on the subtitles

--- subtitle_adder.rb
+++ subtitle_adder.rb
@@ -3,17 +3,10 @@ class SubtitleAdder
   require "rubygems"
   require "bundler/setup"
 
-  # Require gems specified in the Gemfile
-  require "viddl"
-  require "viddl/video/clip"
-  require 'fileutils'
-  require_relative 'medium'
-
   def self.do_your_thing
-    #ffmpeg -i ~/Dropbox\ \(Personal\)/VIRB\ takeouts/VIRB0653.MP4 -vf subtitles=subtitle.srt subtitled.MP4
-
-  
-
+    #ffmpeg -i ~/Dropbox\ \(Personal\)/VIRB\ takeouts/VIRB0653.MP4 -vf subtitles=speeds.srt subtitled.MP4
+    #ffmpeg -i ~/Dropbox\ \(Personal\)/VIRB\ takeouts/VIRB0653.MP4 -vf subtitles=speeds.srt:force_style='Alignment=7' subtitled.MP4
+    system "ffmpeg -i ~/Dropbox\\ \\(Personal\\)/VIRB\\ takeouts/VIRB0653.MP4 -vf subtitles=speeds.srt:force_style='Alignment=7' subtitled.MP4"
   end
 
   SubtitleAdder.do_your_thing
