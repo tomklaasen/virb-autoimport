010a95afb59dcf1faf487e6290d3958120a6529c
Tom Klaasen <tomk@tomk.be>
2017-10-28 14:49:09 +0200

    fixed fit stuff

--- fit_thing.rb
+++ fit_thing.rb
@@ -11,18 +11,14 @@ class FitThing
   def self.do_stuff
     counter = 0
     File.open("speeds.srt", 'w') do  |file|
-      fit_file = Fit.load_file("/Users/tkla/Desktop/VIRB\ dump\ 20171023/GMetrix/2017-10-23-07-12-34.fit")
+      fit_file = Fit.load_file("/Users/tkla/Desktop/VIRB\ dump\ 20171027/GMetrix/2017-10-27-14-05-11.fit")
       records = fit_file.records
       records.each do |r|
         header = r.header
+
         # Look for the GPS messages
-        puts "Header       : #{r.header}"
-        puts "Content      : #{r.content}"
-
-        # This used to work, but not anymore. Dit the file format change on the VIRB??
-
         if header[:header_type] == 0 && header[:message_type] == 0 && header[:local_message_type] == 12
-          puts "r.content: #{r.content}"
+          # puts "r.content: #{r.content}"
           utc_timestamp = r.content[:raw_field_6]
 
             # Apparently, 1989-12-31T00:00:00 is used as 0. That's 631065600 in the Unix epoch
@@ -31,8 +27,10 @@ class FitThing
             speed_km_h = speed_mm_s.to_f / 10000*36
 
             file.write("#{counter + 1}\n")
-            file.write("00:00:#{'%02d' % counter},001 --> 00:00:#{'%02d' % (counter + 1)},000\n")
-            file.write("#{Time.at(unix_timestamp).strftime("%Y-%m-%d %H:%M:%S")}        #{speed_km_h.round} km/h\n\n")
+            start = "#{FitThing.counter_to_time_string(counter)},001"
+            stop = "#{FitThing.counter_to_time_string(counter+1)},000"
+            file.write("#{start} --> #{stop}\n")
+            file.write("#{Time.at(unix_timestamp).strftime("%Y-%m-%d %H:%M:%S")}        #{speed_km_h.round(1)} km/h\n\n")
 
             counter += 1
 
@@ -42,13 +40,22 @@ class FitThing
               # puts "  position_lat       : #{r.content[:raw_field_1]}"
               # puts "  position_long      : #{r.content[:raw_field_2]}"
               # puts "  speed (mm/s)       : #{speed_mm_s}"
-              puts "  speed (km/h)       : #{speed_km_h}"
+              # puts "  speed (km/h)       : #{speed_km_h}"
               # puts "  ISO 8601 timestamp : #{Time.at(unix_timestamp).strftime("%Y%m%dT%H%M%S%z")}"
               # puts "  timestamp_ms : #{r.content[:raw_field_0]}"
               # puts
         end
       end
     end
+  end
+
+  def self.counter_to_time_string(counter)
+    seconds = counter % 60
+    minutes = counter / 60
+    hours = minutes / 60
+    minutes = minutes % 60
+
+    "#{'%02d' % hours}:#{'%02d' % minutes}:#{'%02d' % seconds}"
   end
 end
 
