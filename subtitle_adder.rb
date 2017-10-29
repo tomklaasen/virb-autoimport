760f4b650a13665c5e19a8e561f8478f944609a0
Tom Klaasen <tomk@tomk.be>
2017-10-29 12:12:52 +0100

    remove intermediate speeds.srt file

--- subtitle_adder.rb
+++ subtitle_adder.rb
@@ -1,16 +1,15 @@
 class SubtitleAdder
+  require 'fileutils'
   include Logging
 
   def add_subtitles(video_path, subtitles_path)
-    #ffmpeg -i ~/Dropbox\ \(Personal\)/VIRB\ takeouts/VIRB0653.MP4 -vf subtitles=speeds.srt subtitled.MP4
-    #ffmpeg -i ~/Dropbox\ \(Personal\)/VIRB\ takeouts/VIRB0653.MP4 -vf subtitles=speeds.srt:force_style='Alignment=7' subtitled.MP4
-    # system "ffmpeg -i ~/Dropbox\\ \\(Personal\\)/VIRB\\ takeouts/VIRB0653.MP4 -vf subtitles=speeds.srt:force_style='Alignment=7' subtitled.MP4"
     video = escape_path_for_command_line(video_path)
     subtitles = escape_path_for_command_line(subtitles_path)
     flags = "-loglevel error -hide_banner"
     command = "ffmpeg -i #{video} #{flags} -vf subtitles=#{subtitles}:force_style='Alignment=7' subtitled.MP4"
     logger.debug "Issuing command: #{command}"
     system command
+    FileUtils.rm subtitles_path
   end
 
   def escape_path_for_command_line(path)
