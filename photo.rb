50b27096877fe2b72c788af8abb91766639a8ebb
Tom Klaasen <tom.klaasen@softwarecaptains.be>
2022-06-05 16:33:58 +0200

    new implementation for photo

--- photo.rb
+++ photo.rb
@@ -1,3 +1,32 @@
 class Photo
+ 	require_relative 'logging'
+	include Logging
 	
+	require 'exif'
+	require 'date'
+	require 'time'
+
+	attr_accessor :path
+
+	def initialize(path)
+		@path = path
+	end
+
+	def timestamp
+		parse(get_data.date_time)
+	end
+
+	def get_data
+		Exif::Data.new(IO.read(@path))   # load from string
+	end
+
+	def parse(date_time)
+		date_time.sub!(':', '-')
+		date_time.sub!(':', '-')
+		timezone = Time.now.strftime('%z')
+		logger.debug "Timezone is #{timezone}"
+		#Time.parse("#{date_time} #{timezone}")
+		Time.parse("#{date_time} UTC")
+	end
+
 end
--- test/photo_test.rb
+++ test/photo_test.rb
@@ -6,8 +6,29 @@ require_relative '../photo'
 require "test/unit"
 
 class PhotoTest < Test::Unit::TestCase
+	def setup
+		Config.load_and_set_settings(Config.setting_files("config", "test"))
+	end
+
+	def teardown
+		Dir.foreach('tmp') do |f|
+		  fn = File.join('tmp', f)
+		  File.delete(fn) if f != '.' && f != '..'
+		end
+	end
+
 	def test_initialize
-		photo = Photo.new
+		path = "sample_data/DCIM/102_VIRB/VIRB0668.jpg"
+		photo = Photo.new(path)
 		assert_not_nil(photo)
+		assert_equal(path, photo.path)
+	end
+
+	def test_readtime
+		path = "sample_data/DCIM/102_VIRB/VIRB0668.jpg"
+		photo = Photo.new(path)
+		# 28 Feb 2022 at 17:54
+		assert_equal('2022-02-28 17:54:18 UTC', photo.timestamp.to_s)
+		assert_equal(Time.parse('2022-02-28 17:54:18 UTC'), photo.timestamp)
 	end
 end
